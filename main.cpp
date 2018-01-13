763597a2a6159ec4935429c30c4729a57ab70c24
Lijun Wang <wang283808065@gmail.com>
2018-01-12 21:07:49 -0800

    main

--- main.cpp
+++ main.cpp
@@ -36,6 +36,8 @@ int main(){
 
     initscr();//creates std screen
     raw();//enter raw modal
+    noecho();
+    getch();
     printw("SCORE  %d", score);
     move(0, 50);
     printw("LIVES  %d", lives);
@@ -52,11 +54,8 @@ int main(){
     // mvaddch(6, 5, '@');
 
     for (int i = 0; i < 10; i++){
-        // enemies[i][0] = rand() % 30 + 3;
-        // enemies[i][1] = rand() % 3 + 1;
         enemy[i].setPos((rand() % 30 + 3), (rand() % 3 + 1));
         attron(A_STANDOUT | A_UNDERLINE);
-        // mvprintw(randam_enemies_y, randam_enemies_x, "@");
         mvprintw(enemy[i].getY(), enemy[i].getX(), "@");
         attroff(A_STANDOUT | A_UNDERLINE);
     }
@@ -73,22 +72,15 @@ int main(){
             mvprintw(enemy[i].getY(), enemy[i].getX(), "@");
             attroff(A_STANDOUT | A_UNDERLINE);
         }
+        start_color();
+        init_pair(1, COLOR_BLUE, COLOR_GREEN);
+
+        attron(COLOR_PAIR(1));
+        mvaddch(20, 25, 'P');
+        attroff(COLOR_PAIR(1));
         sleep(2);
         refresh();
     }
-    
-
-
-    // move(60, 50);
-
-    start_color();
-    init_pair(1, COLOR_BLUE, COLOR_GREEN);
-
-    attron(COLOR_PAIR(1));
-    mvaddch(20, 25, 'P');
-    attroff(COLOR_PAIR(1));
-
-    getch();
     endwin();
 
     return 0;
